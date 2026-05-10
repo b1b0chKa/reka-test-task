4f31c65b94eda671f9b55b6d014d95bc69ed2c9b
Daniil <teemfase@gmail.com>
2026-05-10 22:50:21 +0300

    refactor(frontend): update layout, routing and vite configuration

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
+
+Route::view('/login', 'auth.login');
+Route::view('/register', 'auth.register');
+
+Route::view('/tags', 'tags.index');
+Route::view('/tasks', 'tasks.index');
